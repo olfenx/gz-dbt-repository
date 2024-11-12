073450a2183fb59e071eca316ad2f022f03cd377
Florian <florian.amardel@gmail.com>
2024-11-12 16:04:28 +0000

    Add sales margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,5 +1,7 @@
 SELECT
 *
+,ROUND(purchase_price * quantity,2) AS purchase_cost
+,ROUND(revenue - (purchase_price * quantity),2) as margin
 FROM {{ ref('stg_gz_raw_data__raw_gz_sales') }}
 JOIN
 {{ ref('stg_gz_raw_data__raw_gz_product') }}
